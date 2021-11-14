775f01487d705487605d73519eada2a93c8d4102
jerrywonggithub <70314892+jerrywonggithub@users.noreply.github.com>
2021-11-14 18:32:39 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,32 +7,26 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
-  
-    componentDidMount() {
 
+  componentDidMount() {
     const PLAYBACK_URL = 'https://3eda70444b8a.us-west-2.playback.live-video.net/api/video/v1/us-west-2.135709585800.channel.TyrzzzBD9Z8t.m3u8';
-
-    // Register Amazon IVS as playback technology for Video.js
-    registerIVSTech(videojs);
-
-    // Initialize player
-    const player = videojs('amazon-ivs-videojs', {
-      techOrder: ["AmazonIVS"]
-    }, () => {
-      console.log('Player is ready to use!');
-      // Play stream
-      player.src(PLAYBACK_URL);
-    })
+    if (window.IVSPlayer && window.IVSPlayer.isPlayerSupported) {
+      console.log('ivsplayer supported');
+      const player = window.IVSPlayer.create();
+      player.attachHTMLVideoElement(document.getElementById('video-player'));
+      player.load(PLAYBACK_URL);
+      player.play();
+    }
   }
-  
+
   render() {
     return (
       <div className="App">
         <AmplifySignOut />
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <video id="video-player" playsinline></video>
           <p>
-            This is our fist demo html for SA launch.
+            This is our first demo html for SA launch.
           </p>
           <a
             className="App-link"
