dafb0c6b1026877d60f7349507c3bf0b8d23c665
jerrywonggithub <70314892+jerrywonggithub@users.noreply.github.com>
2021-11-14 18:08:13 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,24 @@ import aws_exports from './aws-exports';
 Amplify.configure(aws_exports);
 
 class App extends Component {
+  
+    componentDidMount() {
+
+    const PLAYBACK_URL = 'https://3eda70444b8a.us-west-2.playback.live-video.net/api/video/v1/us-west-2.135709585800.channel.TyrzzzBD9Z8t.m3u8';
+
+    // Register Amazon IVS as playback technology for Video.js
+    registerIVSTech(videojs);
+
+    // Initialize player
+    const player = videojs('amazon-ivs-videojs', {
+      techOrder: ["AmazonIVS"]
+    }, () => {
+      console.log('Player is ready to use!');
+      // Play stream
+      player.src(PLAYBACK_URL);
+    })
+  }
+  
   render() {
     return (
       <div className="App">
